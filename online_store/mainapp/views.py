81d25fa832e4b3fde27a739c49f257dd1ef4696b
tritium <tinotiano@gmail.com>
2021-10-06 00:56:11 +0300

    наследование шаблонов и подключение подшаблонов

--- online_store/mainapp/views.py
+++ online_store/mainapp/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Продукты'
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    slider_products = (
+        {'img': 'online_store/img/controll.jpg', 'text': ''},
+        {'img': 'online_store/img/controll1.jpg', 'text': ''},
+        {'img': 'online_store/img/controll2.jpg', 'text': ''}
+    )
+    description_list = (
+        'Расположитесь комфортно.',
+        'Отличное качество материалов позволит вам это.',
+        'Различные цвета',
+        'высочайший уровень эргономики и прочность.'
+    )
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+        'slider_products': slider_products,
+        'description_list': description_list
+    }
+    return render(request, 'mainapp/products.html', context=context)
--- online_store/online_store/views.py
+++ online_store/online_store/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'online_store/index.html')
+    title = 'магазин'
+    context = {
+        'title': title,
+    }
+    return render(request, 'online_store/index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'online_store/contact.html')
+    title = 'Контакты'
+    contact_list = (
+        {'city': 'Москва', 'tel': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+        {'city': 'Москва', 'tel': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+        {'city': 'Москва', 'tel': '+7-888-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'}
+    )
+    context = {
+        'title': title,
+        'contacts': contact_list
+    }
+    return render(request, 'online_store/contact.html', context=context)

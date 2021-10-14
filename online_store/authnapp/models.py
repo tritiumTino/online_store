6c847d4b0260bac1bc869a8e87f0958444454ded
tritium <tinotiano@gmail.com>
2021-10-14 21:30:46 +0300

    обновление шаблонов профиля

--- online_store/authnapp/models.py
+++ online_store/authnapp/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from PIL import Image
+
+
+_MAX_SIZE = 150
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='аватар')
     age = models.PositiveIntegerField(verbose_name='возраст')
+
+    def save(self, *args, **kwargs):
+        super(ShopUser, self).save(*args, **kwargs)
+        if self.avatar:
+            img = Image.open(self.avatar.path)
+            if img.height > _MAX_SIZE or img.width > _MAX_SIZE:
+                img = img.resize(
+                    (_MAX_SIZE, _MAX_SIZE),
+                    Image.ANTIALIAS
+                )
+                img.save(self.avatar.path)
--- online_store/authnapp/urls.py
+++ online_store/authnapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'authnapp'
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('register/', register, name='register'),
+    path('edit/', edit, name='edit'),
 ]

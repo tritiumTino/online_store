a8b50d01c0f215617941cf5d809234020f8c0717
tritium <tinotiano@gmail.com>
2021-11-09 22:30:25 +0300

    sending verification email

--- online_store/authnapp/forms.py
+++ online_store/authnapp/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 from .models import ShopUser
+import random
+import hashlib
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -29,6 +31,15 @@ class ShopUserRegisterForm(UserCreationForm):
         if data < 18:
             raise forms.ValidationError("Вы слишком молоды!")
         return data
+
+    def save(self):
+        user = super(ShopUserRegisterForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        user.save()
+        return user
 
 
 class ShopUserEditForm(UserChangeForm):

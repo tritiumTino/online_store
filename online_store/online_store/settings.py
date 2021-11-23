cd5a8dc4f4dc819ccbf45b455561be44f61614b6
tritium <tinotiano@gmail.com>
2021-11-24 00:30:42 +0300

    deploy

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'online_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
